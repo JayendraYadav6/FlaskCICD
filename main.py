7b2eeb59e462c9caac99a9ff78b5543e3263252d
Jayendra Yadav <90862406+JayendraYadav6@users.noreply.github.com>
2025-05-20 16:52:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import boto3
+import requests
+ 
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return"hello This is your buddy apoorv"
+ 
+if __name__ =="__main__":
+  app.run(host="0.0.0.0" ,port=5002)
