f31cf9d70129acae513288dfe135837da09bf964
Jayendra Yadav <90862406+JayendraYadav6@users.noreply.github.com>
2025-05-28 14:01:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return"hello This is your buddy jayendra yadav"
+    return"hello This is your buddy jayendra "
  
 if __name__ =="__main__":
   app.run(host="0.0.0.0" ,port=5002)
